ae3b4eb947225afd721e17d802de77a8ad225745
Ankit <ankitchandra25sept@gmail.com>
2024-11-23 20:57:17 +0530

    Set the stage for combat

--- Pokemon.cpp
+++ Pokemon.cpp
@@ -6,30 +6,62 @@ using namespace std;
 
 // Default constructor
 Pokemon::Pokemon() {
-	name = "Unknown";
-	type = PokemonType::NORMAL;
-	health = 50;
+    name = "Unknown";
+    type = PokemonType::NORMAL;
+    health = 50;
 }
 
 // Parameterized constructor
 Pokemon::Pokemon(string p_name, PokemonType p_type, int p_health) {
-	name = p_name;
-	type = p_type;
-	health = p_health;
+    name = p_name;
+    type = p_type;
+    health = p_health;
 }
 
 // Copy constructor
 Pokemon::Pokemon(const Pokemon& other) {
-	name = other.name;
-	type = other.type;
-	health = other.health;
+    name = other.name;
+    type = other.type;
+    health = other.health;
 }
 
 // Destructor
 Pokemon::~Pokemon() {
-	// Destructor logic (if any) goes here
+    // Destructor logic (if any) goes here
 }
 
-void Pokemon::attack() {
-	cout << name << " attacks with a powerful move!\n";
+void Pokemon::attack(Pokemon& target) {
+    int damage = 10; // Fixed damage for simplicity
+    cout << name << " attacks " << target.name << " for " << damage << " damage!\n";
+    target.takeDamage(damage); // Apply damage to the target Pokémon
+}
+
+void Pokemon::takeDamage(int damage) {
+    health -= damage; // Reduce HP by the damage amount
+    if (health < 0) {
+        health = 0; // Ensure HP doesn't go below 0
+    }
+}
+
+bool Pokemon::isFainted() const {
+    return health <= 0; // Return true if HP is 0 or less
+}
+
+void battle(Pokemon& playerPokemon, Pokemon& wildPokemon) {
+    cout << "A wild " << wildPokemon.name << " appeared!\n";
+
+    while (!playerPokemon.isFainted() && !wildPokemon.isFainted()) {
+        playerPokemon.attack(wildPokemon); // Player attacks first
+
+        if (!wildPokemon.isFainted()) {
+            wildPokemon.attack(playerPokemon); // Wild Pokémon attacks back
+        }
+    }
+
+    if (playerPokemon.isFainted()) {
+        cout << playerPokemon.name << " has fainted! You lose the battle.\n";
+    }
+    else {
+        cout << "You defeated the wild " << wildPokemon.name << "!\n";
+    }
 }
--- Pokemon.hpp
+++ Pokemon.hpp
@@ -9,6 +9,7 @@ public:
     string name;
     PokemonType type;
     int health;
+    int maxHealth; // Represents the maximum HP
 
     // Default constructor
     Pokemon();
@@ -16,11 +17,14 @@ public:
     // Parameterized constructor
     Pokemon(string p_name, PokemonType p_type, int p_health);
 
+    // Method declarations
+    void attack(Pokemon& target); // Declare attack method here
+    void takeDamage(int damage); // Method to reduce HP
+    bool isFainted() const; // Method to check if the Pokemon has fainted
+
     // Copy constructor
     Pokemon(const Pokemon& other);
 
     // Destructor
     ~Pokemon();
-
-    void attack();
 };
